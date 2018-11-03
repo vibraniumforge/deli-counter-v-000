d14ef2acbca0f023e46eb2b1397cdda315da29ca
Greg Morgan <gregm9999@gmail.com>
2018-11-03 00:33:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length==0
     puts "The line is currently empty."
   else
-      array.each_with_index.map do |item, index| puts "The line is currently: #{ index+1}. #{item}"
+      array.each_with_index do |item, index| puts "The line is currently: #{ index+1}. #{item}"
       end
   end
 end

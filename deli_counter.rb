e033450f8dd066b1a81b9436aaab09f8a991221b
Greg Morgan <gregm9999@gmail.com>
2018-11-03 00:10:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-   "Welcome, #{name}. You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)

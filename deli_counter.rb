fba6dc48ad0876cfb55123face318f299e6a9408
Greg Morgan <gregm9999@gmail.com>
2018-11-03 00:08:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require 'pry'
+def line(array)
+  if array.length==0
+    puts "The line is currently empty."
+  else
+    # while counter < array.length
+    #   counter=0
+    #   puts "The line is currently: #{counter+1}. #{array[counter] }"
+    #   counter+=
+      array.each_with_index.map do |item, index| puts "The line is currently: #{ index+1}. #{item}"
+        binding.pry
+      end
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
